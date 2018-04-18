26521c3fb5b07abe97c10baae2c85475782e493d
douangtavanh <32052391+douangtavanh@users.noreply.github.com>
2018-04-19 01:31:00 +0700

    support api 2
    edit all code to support new api 2

--- index.js
+++ index.js
@@ -1,34 +1,29 @@
 'use strict';
 
-process.env.DEBUG = 'actions-on-google:*';
-const { DialogflowApp } = require('actions-on-google');
+const {
+  dialogflow,
+  Suggestions,
+  BasicCard,
+  Image,
+} = require('actions-on-google');
 const functions = require('firebase-functions');
+
+const app = dialogflow({debug: true});
+
+
 const data = require('./data');
 const quizData = require('./quiz-data');
 const images = require('./images');
 const sprintf = require('sprintf-js').sprintf;
 
-//Dialogflow variable
-const intent = {
-  welcome: 'welcome',
-  fact: 'fact',
-  quiz: 'quiz',
-  factYes: 'factYes',
-  factNo: 'factNo',
-  about: 'about',
-}
-
-const argument = {
-  Yes: 'Yes',
-  No: 'No',
-  Fact: 'Fact',
-  Quiz: 'Quiz',
-}
-
-const context = {
-  fact: 'fact',
-  welcome: 'welcome',
-}
+//Check screen availavble
+app.middleware((conv) => {
+  conv.hasScreen =
+    conv.surface.capabilities.has('actions.capability.SCREEN_OUTPUT');
+  conv.hasAudioPlayback =
+    conv.surface.capabilities.has('actions.capability.AUDIO_OUTPUT');
+});
+    
 
 //Copy facts data from original
 const importFact = data.fact;
@@ -43,106 +38,150 @@ for (let i in quizData.data) {
 let question = coneQuiz;
 
 //Functions
-const delFact = array => {
-  factData.splice(factData.indexOf(array), 1);
-}
-
 const randomValue = array => {
   return array[Math.floor(Math.random() * array.length)];
 }
 
-const remainFact = app => {
-  if ((Boolean(factData.length === 0) === (true))) {
-    factData = data.fact.slice();
-    app.tell('I think I run out of fact. Please talk to me again or keep coming back later.');
-  }
+const delFact = array => {
+  factData.splice(factData.indexOf(array), 1);
 }
 
-const remainQuiz = app => {
+//Main intent processing
+app.intent('welcome', conv => {
+  conv.ask(randomValue(data.welcome.intent));
+  conv.ask(new Suggestions(['Fact', 'Quick quiz']));
+});
+
+app.intent('fact', conv => {
+  if ((Boolean(factData.length === 0) === (true))) {
+    factData = data.fact.slice();
+    conv.close('I think I run out of fact. Please talk to me again or keep coming back later.');
+  } else {
+    let random = randomValue(factData);
+  if (!conv.hasScreen) {
+    conv.ask(random + '. do you want more fact?');
+  } else {
+    let findFact = element => {
+      return element === random;
+    }
+    if ((images.images[importFact.findIndex(findFact)]) === null) {
+      conv.ask(random);
+      conv.ask('Do you want more facts?');
+      conv.ask(new Suggestions(['Yes', 'No']));
+    } else {
+      conv.ask(random);
+      conv.ask(new BasicCard({
+        text: random,
+        title: images.imagesTitle[importFact.findIndex(findFact)],
+        image: new Image({
+          url: images.images[importFact.findIndex(findFact)],
+          alt: images.imagesTitle[importFact.findIndex(findFact)],
+        })
+      }));
+    conv.ask('Do you want more facts?');
+    conv.ask(new Suggestions(['Yes', 'No']));
+    }
+  }
+  factData.splice(factData.indexOf(random), 1);
+  }
+});
+
+app.intent('factYes', conv => {
+  if ((Boolean(factData.length === 0) === (true))) {
+    factData = data.fact.slice();
+    conv.close('I think I run out of fact. Please talk to me again or keep coming back later.');
+  } else {
+    let random = randomValue(factData);
+  if (!conv.hasScreen) {
+    conv.ask(random + '. do you want more fact?');
+  } else {
+    let findFact = element => {
+      return element === random;
+    }
+    if ((images.images[importFact.findIndex(findFact)]) === null) {
+      conv.ask(random);
+      conv.ask('Do you want more facts?');
+      conv.ask(new Suggestions(['Yes', 'No']));
+    } else {
+      conv.ask(random);
+      conv.ask(new BasicCard({
+        text: random,
+        title: images.imagesTitle[importFact.findIndex(findFact)],
+        image: new Image({
+          url: images.images[importFact.findIndex(findFact)],
+          alt: images.imagesTitle[importFact.findIndex(findFact)],
+        })
+      }));
+    conv.ask('Do you want more facts?');
+    conv.ask(new Suggestions(['Yes', 'No']));
+    }
+  }
+  factData.splice(factData.indexOf(random), 1);
+  }
+});
+
+app.intent('factNo', conv => {
+  conv.close('I hope to see you next time. Have a nice day!');
+});
+
+app.intent('quiz', conv => {
   if ((Boolean(Object.keys(question).length === 0)) === (true)) {
     coneQuiz = {};
     for (let x in quizData.data) {
       coneQuiz[x] = quizData.data[x];
     }
     question = coneQuiz;
-    app.tell('You already got it all');
+    conv.close('I run out of questions. Please come back later.');
+  } else {
+    let randomQuiz = randomValue(Object.getOwnPropertyNames(question));
+    conv.ask((Object.getOwnPropertyDescriptor(question, randomQuiz).value));
+    conv.data.question = randomQuiz;
+    delete question[randomQuiz];
   }
-}
-
-//Main intent processing
-const welcome = app => {
-  app.ask(app.buildRichResponse().addSimpleResponse(randomValue(data.welcome.intent)).addSuggestions(['Fact', 'Quick Quiz']));
-}
-const fact = app => {
-  const hasScreen = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
-  remainFact(app);
-  let random = randomValue(factData);
-  if (!hasScreen) {
-    app.ask(random + '. do you want more fact?');
-  }
-  //Check array index
-  function findFact(element) {
-    return element === random;
-  }
-  if ((images.images[importFact.findIndex(findFact)]) === null) {
-    app.ask(app.buildRichResponse().addSimpleResponse(random + '. do you want more fact?').addSuggestions(['Yes', 'No']));
-  }
-  app.ask(app.buildRichResponse().addSimpleResponse(random + '. do you want more fact?').addSuggestions(['Yes', 'No']).addBasicCard(app.buildBasicCard(random).setTitle(images.imagesTitle[importFact.findIndex(findFact)]).setImage(images.images[importFact.findIndex(findFact)], images.imagesTitle[importFact.findIndex(findFact)])));
-  factData.splice(factData.indexOf(random), 1);
-}
-
-const factYes = app => {
-  const hasScreen = app.hasSurfaceCapability(app.SurfaceCapabilities.SCREEN_OUTPUT);
-  remainFact(app);
-  let random = randomValue(factData);
-  if (!hasScreen) {
-    app.ask(random + '. do you want more fact?');
-  }
-  //Check array index
-  function findFact(element) {
-    return element === random;
-  }
-  if ((images.images[importFact.findIndex(findFact)]) === null) {
-    app.ask(app.buildRichResponse().addSimpleResponse(random + '. do you want more fact?').addSuggestions(['Yes', 'No']));
-  }
-  app.ask(app.buildRichResponse().addSimpleResponse(random + '. do you want more fact?').addSuggestions(['Yes', 'No']).addBasicCard(app.buildBasicCard(random).setTitle(images.imagesTitle[importFact.findIndex(findFact)]).setImage(images.images[importFact.findIndex(findFact)], images.imagesTitle[importFact.findIndex(findFact)])));
-  factData.splice(factData.indexOf(random), 1);
-}
-
-const factNo = app => {
-  app.tell('I hope to see you next time. Have a nice day!');
-}
-
-const quiz = app => {
-  //app.tell('I\'m sorry, my quick quiz is not available yet, meanwhile, I can tell you only fact.');
-  remainQuiz(app);
-  let randomQuiz = randomValue(Object.getOwnPropertyNames(question));
-  app.ask((Object.getOwnPropertyDescriptor(question, randomQuiz).value));
-  delete question[randomQuiz];
-}
-
-const about = app => {
-  app.ask('Lao Youth Facts will tell you the random facts about SEAYP, ASEAN, Lao PDR facts. Therefore, It\'s might help you to prepare for SSEAYP exam. Do you want to hear facts?');
-}
-
-//Intent map
-const actionMap = new Map();
-actionMap.set(intent.welcome, welcome);
-actionMap.set(intent.fact, fact);
-actionMap.set(intent.quiz, quiz);
-actionMap.set(intent.factYes, factYes);
-actionMap.set(intent.factNo, factNo);
-actionMap.set(intent.about, about);
-
-
-//Dialogflow output
-const laoYouth = functions.https.onRequest((request, response) => {
-  const app = new DialogflowApp({ request, response });
-  console.log(`Request headers: ${JSON.stringify(request.headers)}`);
-  console.log(`Request body: ${JSON.stringify(request.body)}`);
-  app.handleRequest(actionMap);
 });
 
-module.exports = {
-  laoYouth
-};
+app.intent('quizYes', conv => {
+  if ((Boolean(Object.keys(question).length === 0)) === (true)) {
+    coneQuiz = {};
+    for (let x in quizData.data) {
+      coneQuiz[x] = quizData.data[x];
+    }
+    question = coneQuiz;
+    conv.close('I run out of questions. Please come back later.');
+  } else {
+    conv.data.guess = 0;
+    let randomQuiz = randomValue(Object.getOwnPropertyNames(question));
+    conv.ask((Object.getOwnPropertyDescriptor(question, randomQuiz).value));
+    conv.data.question = randomQuiz;
+    delete question[randomQuiz];
+  }
+});
+
+app.intent('q1', conv => {
+  let input = conv.parameters['prime_minister'];
+  let answer = Object.getOwnPropertyDescriptor(quizData.answer, conv.data.question).value;
+  if ((input.toLocaleLowerCase() === answer.toLocaleLowerCase())) {
+    conv.ask('Your answer is correct!');
+    conv.ask('Do you want more quiz?');
+  } else {
+    conv.ask('You\'re so close. Please try again.');
+  }
+});
+
+app.intent('q2', conv => {
+  let input = conv.parameters['number'];
+  let answer = Object.getOwnPropertyDescriptor(quizData.answer, conv.data.question).value;
+  if (input.toString() === answer) {
+    conv.ask('Your answer is correct!');
+    conv.ask('Do you want more quiz?');
+  } else {
+    conv.ask('You\'re so close. Please try again.');
+  }
+})
+
+app.intent('about', conv => {
+  conv.ask('Lao Youth Facts will tell you the random facts about SEAYP, ASEAN, Lao PDR facts. Therefore, It\'s might help you to prepare for SSEAYP exam. Do you want to hear facts?');
+});
+
+//Dialogflow output
+exports.laoYouth = functions.https.onRequest(app);
